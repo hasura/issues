82e45141fe26668e339e90d7937372184c951f6b
Tanmai Gopal <tanmaig@34cross.in>
2016-07-01 02:46:36 +0530

    Adds README and sample docker run file

---
+++ README.md
@@ -0,0 +1,12 @@
+#To install
+```
+cd app
+npm install
+```
+
+#To run
+Configure ``env`` variables in ``sample.rundocker.sh``.
+```
+./sample.rundocker.sh
+```
+Head to ``http://<docker-host>:7030/milestone/<milestoneName>``
--- sample.rundocker.sh
+++ sample.rundocker.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# gitlab issue API allows fetch by milestone name
+# github issue API needs milestone id. That means milestone-id that corresponds to milestone-name needs to be configured 
+# for EVERY GITHUB PROJECT
+projects='{"gitlab": ["NAMESPACE/PROJECT"],
+"github": [{"name": "USER/REPO", "milestones":{"MILESTONE": GITHUB_MILESTONE_ID}]}'
+gitlab='<insert-token-here>'
+github='<insert-token-here>'
+deadlines='{"MILESTONE": {"start": "2016-06-24", "end": "2016-07-08"}}'
 
-projects='{"gitlab": ["NAMEPSACE/PROJECT"],"github": ["USER/REPO"]}'
-gitlab=''
-github=''
+#Users often have differnt usernames on github & gitlab. Create a map to track users by a single name (REALUSERNAME)
+users='{"githubUsername1": "REALUSERNAME_1", "gitlabUsername1": "REALUSERNAME_1", "gitlabUsername2": "REALUSERNAME_2"}'
 
-docker run --rm -t -i -e PROJECTS="$projects" -e GITHUB=$github -e GITLAB=$gitlab -p 7030:80 \
+docker run --rm -t -i -e PROJECTS="$projects" -e DEADLINES="$deadlines" -e USERS="$users" -e GITHUB=$github -e GITLAB=$gitlab -p 7030:80 \
        -v $(pwd)/app:/app \
        hasura/issues
