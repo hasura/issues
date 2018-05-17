4d68e06990099704dfa12f90049dc485b069c3d3
Tanmai Gopal <tanmaig@34cross.in>
2018-05-17 15:26:14 -0700

    removes hasura specific cron

--- microservices/cron/src/main.py
+++ microservices/cron/src/main.py
@@ -7,9 +7,6 @@ my_cron = CronTab(tabfile='my_cron.tab')
 job  = my_cron.new(command='python /usr/src/app/save.py')
 job.hour.every(12)
 
-job2  = my_cron.new(command='python /usr/src/app/save_hub.py')
-job2.hour.every(12)
-
 # Write cron jobs to cron tab file
 my_cron.write()
 
--- microservices/cron/src/save_hub.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-res = requests.get('http://app.default/save_hub_pulls_snapshot/today');
-print (res.text)
